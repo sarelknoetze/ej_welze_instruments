cff7bcbff6da6a91d7bf6f7d6bff9aa8514082ba
sarelknoetze <122682873+sarelknoetze@users.noreply.github.com>
2023-03-01 18:23:25 +0200

    Add files via upload

---
+++ contact.js
@@ -0,0 +1,20 @@
+function eCall(){
+    window.open('tel:+27814753717');
+}
+
+function jCall(){
+    window.open('tel:+27814428757');
+}
+
+function eEmail(){
+    window.open('mailto:Ejwelzeinstruments@gmail.com');
+}
+
+function eWhatsapp(){
+    // window.open('whatsapp:+27814753717')
+    window.open('https://api.whatsapp.com/send?phone=+27814753717&text=hi');
+}
+
+function jWhatsapp(){
+    window.open('https://api.whatsapp.com/send?phone=+27814428757&text=hi');
+}
---
+++ index.js
@@ -0,0 +1,73 @@
+
+$(".hover1").hover(function () {
+    $(".heroSection").css({ "background-image": "url(./images/try.jpg)", "background-position": "center", "color": "black" })
+});
+
+$(".hover1").mouseleave(function () {
+    $(".heroSection").css({ "background-image": "none", "color": "white" })
+});
+
+$(".hover2").hover(function () {
+    $(".heroSection").css({ "background-image": "url(./images/vegaLevel.jpeg)", "background-position": "center", "color": "black" })//sit jou foto in vir here section background
+});
+
+$(".hover2").mouseleave(function () {
+    $(".heroSection").css({ "background-image": "none", "color": "white" })
+});
+
+$(".hover3").hover(function () {
+    $(".heroSection").css({ "background-image": "url(./images/YokogawaPressureTransmitters.jpeg)", "background-position": "center", "color": "black" })//sit jou foto in vir here section background
+});
+
+$(".hover3").mouseleave(function () {
+    $(".heroSection").css({ "background-image": "none", "color": "white" })
+});
+
+$(".hover4").hover(function () {
+    $(".heroSection").css({ "background-image": "url(./images/TemperatureTransmitters.jpeg)", "background-position": "center", "color": "black" })//sit jou foto in vir here section background
+});
+
+$(".hover4").mouseleave(function () {
+    $(".heroSection").css({ "background-image": "none", "color": "white" })
+});
+
+$(".hover5").hover(function () {
+    $(".heroSection").css({ "background-image": "url(./images/camera.jpeg)", "background-position": "center", "color": "black" })//sit jou foto in vir here section background
+});
+
+$(".hover5").mouseleave(function () {
+    $(".heroSection").css({ "background-image": "none", "color": "white" })
+});
+
+$(".hover6").hover(function () {
+    $(".heroSection").css({ "background-image": "url(./images/thicknessIndicators.jpeg)", "background-position": "center", "color": "black" })//sit jou foto in vir here section background
+});
+
+$(".hover6").mouseleave(function () {
+    $(".heroSection").css({ "background-image": "none", "color": "white" })
+});
+
+$(".hover7").hover(function () {
+    $(".heroSection").css({ "background-image": "url(./images/foxboroMetal.jpeg)", "background-position": "center", "color": "black" })//sit jou foto in vir here section background
+});
+
+$(".hover7").mouseleave(function () {
+    $(".heroSection").css({ "background-image": "none", "color": "white" })
+});
+
+$(".hover8").hover(function () {
+    $(".heroSection").css({ "background-image": "url(./images/plc.jpeg)", "background-position": "center", "color": "black" })//sit jou foto in vir here section background
+});
+
+$(".hover8").mouseleave(function () {
+    $(".heroSection").css({ "background-image": "none", "color": "white" })
+});
+
+$(".hover9").hover(function () {
+    $(".heroSection").css({ "background-image": "url(./images/yokogawaPhMeters.jpeg)", "background-position": "center", "color": "black" })//sit jou foto in vir here section background
+});
+
+$(".hover9").mouseleave(function () {
+    $(".heroSection").css({ "background-image": "none", "color": "white" })
+});
+
